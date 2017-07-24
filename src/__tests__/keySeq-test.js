59a1c9415a30d6b3b06a5586ed23cdde96963f54
Colby Rabideau <crabideau@hubspot.com>
2017-07-24 13:59:26 -0400

    fix keySeq

--- src/__tests__/keySeq-test.js
+++ src/__tests__/keySeq-test.js
@@ -2,12 +2,12 @@ import { Map } from "immutable";
 import keySeq from "../keySeq";
 
 describe("transmute/keySeq", () => {
-  it("gets the keys from a Map", () => {
-    expect(keySeq(Map({ one: 1, two: 2, three: 3 }))).toMatchSnapshot();
-  });
-
   it("gets the keys from an Array", () => {
     expect(keySeq([1, 2, 3])).toMatchSnapshot();
+  });
+
+  it("gets the keys from a Map", () => {
+    expect(keySeq(Map({ one: 1, two: 2, three: 3 }))).toMatchSnapshot();
   });
 
   it("gets the keys from an Object", () => {
--- src/keySeq.js
+++ src/keySeq.js
@@ -1,13 +1,9 @@
 import { Iterable, Seq } from "immutable";
 import { keySeq } from "./protocols/Keyed";
 
-keySeq.implement(Iterable, subject => {
-  return subject.keySeq();
-});
+keySeq.implementInherited(Object, subject => Seq(subject).keySeq());
 
-keySeq.implement(Object, subject => {
-  return Seq(subject).keySeq();
-});
+keySeq.implementInherited(Iterable, subject => subject.keySeq());
 
 /**
  * Get a Seq of the keys in `value`.
