14014dcd115ce1502993aa0bd9213c495cf885fd
Colby Rabideau <crabideau@hubspot.com>
2017-08-14 16:01:14 -0400

    count

---
+++ src/count.js
@@ -0,0 +1,16 @@
+import {count} from './protocols/Countable';
+import {Iterable} from 'immutable';
+
+count.implement(Array, arr => arr.length);
+
+count.implementInherited(Iterable, subject => subject.count());
+
+count.implement(Object, obj => Object.keys(obj).length);
+
+/**
+ * Returns the number of values in `subject`.
+ * 
+ * @param {TYPE} subject
+ * @return {number}
+ */
+export default count;
