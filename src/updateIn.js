59da33c62fa77ce450c3705f73cd3d0038d777cc
Colby Rabideau <crabideau@hubspot.com>
2017-10-24 11:26:50 -0400

    example of undefined keypath setIn/updateIn

--- src/updateIn.js
+++ src/updateIn.js
@@ -15,6 +15,14 @@ function updateIn(keyPath, updater, subject) {
  * incrementUserCount({users: {count: 1}});
  * // returns {users: {count: 2}}
  *
+ * @example <caption>Unset keyPaths will be set based on the most recent type.</caption>
+ * const incrementUserCount = updateIn(['users', 'count'], (n = 0) => n + 1);
+ * incrementUserCount({});
+ * // returns {users: {count: 1}}
+ *
+ * incrementUserCount(Map());
+ * // returns Map { users => Map { count => 1 } }
+ *
  * @param  {Array<any>|Iterable<any>} keyPath the location where `updater` should be applied.
  * @param  {Function} updater the tranformation to apply.
  * @param  {Array|Iterable|Object} subject the thing to update.
